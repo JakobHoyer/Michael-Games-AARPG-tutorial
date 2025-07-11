6dad322d8a73764de76d92e23a3ed07dc451ffe7
JakobHoyer <jakob.hoyer@gmail.com>
2025-07-11 08:06:45 +0200

    Saving now includes your items

--- 00_Globals/global_save_manager.gd
+++ 00_Globals/global_save_manager.gd
@@ -13,7 +13,7 @@ var current_save : Dictionary = {
 		position_x = 0,
 		position_y = 0,
 	},
-	items = [],
+	inventory = [],
 	persistence = [],
 	quests = [],
 }
@@ -22,12 +22,12 @@ var current_save : Dictionary = {
 func save_game() -> void:
 	update_scene_path()
 	update_player_data()
+	update_item_data()
 	var file := FileAccess.open(SAVE_PATH + "save.sav", FileAccess.WRITE)
 	var save_json = JSON.stringify(current_save)
 	file.store_line(save_json)
 	game_saved.emit()
 	print("save_game")
-	pass
 
 
 func load_game() -> void:
@@ -43,12 +43,11 @@ func load_game() -> void:
 	var player_position : Vector2 = Vector2(current_save.player.position_x, current_save.player.position_y)
 	PlayerManager.set_player_position(player_position)
 	PlayerManager.set_health(current_save.player.hp, current_save.player.max_hp)
-	
+	PlayerManager.INVENTORY_DATA.parse_save_data(current_save.items)
 	await LevelManager.level_loaded
 	game_loaded.emit()
 	
 	print("load_game")
-	pass
 
 
 func update_player_data() -> void:
@@ -57,7 +56,6 @@ func update_player_data() -> void:
 	current_save.player.max_hp = p.max_hp
 	current_save.player.position_x = p.global_position.x
 	current_save.player.position_y = p.global_position.y
-	pass
 
 func update_scene_path() -> void:
 	# find the path by finding the level node
@@ -66,4 +64,7 @@ func update_scene_path() -> void:
 		if c is Level: # should only have one (as we release others the player is not on)
 			path = c.scene_file_path
 	current_save.scene_path = path
-	pass
+
+
+func update_item_data() -> void:
+	current_save.items = PlayerManager.INVENTORY_DATA.get_save_data()
